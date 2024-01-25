da40c1f5587aaad0c8930bd2a478eaaec3c26fca
Jamshidbek Makhmudov <101079499+Jamshidbek-Makhmudov@users.noreply.github.com>
2024-01-25 09:00:39 +0900

    added toggle color mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import '../public/css/App.css'
 import { Grid } from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Grid>
